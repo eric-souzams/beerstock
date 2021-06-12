5918f7caba2124ce9ddf03542b16642d4fb7b58f
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-06-12 16:50:06 -0300

    wip: add entity, DTOs and enums

---
+++ src/main/java/com/project/beerstock/enums/BeerType.java
@@ -0,0 +1,19 @@
+package com.project.beerstock.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BeerType {
+
+    LAGER("Lager"),
+    MALZBIER("Malzbier"),
+    WITBIER("Witbier"),
+    WEISS("Weiss"),
+    ALE("Ale"),
+    IPA("IPA"),
+    STOUT("Stout");
+
+    private String description;
+}
