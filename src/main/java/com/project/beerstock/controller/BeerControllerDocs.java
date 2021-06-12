bf7bee3800451d1e4388e3eb758195004a4d4f62
Eric Magalhaes <15831837+eric-souzams@users.noreply.github.com>
2021-06-12 17:20:23 -0300

    wip: fix some bugs

--- src/main/java/com/project/beerstock/controller/BeerControllerDocs.java
+++ src/main/java/com/project/beerstock/controller/BeerControllerDocs.java
@@ -10,7 +10,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Api("Manages beer stock")
